18584bf3b0b78aedbf339a896d2abfcf8e248f06
Daniel Dyssegaard Kallick <ddyssegaardk@gmail.com>
2017-03-01 17:37:26 -0500

    add npm, babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,23 @@ module.exports = {
   output: {
     filename: "./lib/bundle.js"
   },
-  devtool: 'source-map'
-}
+  resolve: {
+    extensions: ['', '.js']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015'],
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  externals: {
+    "createjs": "createjs",
+  },
+};
